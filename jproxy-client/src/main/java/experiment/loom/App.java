5bc6f07f3ac8cb8bc5d7af03908566119d4d90fa
Moshlehuddin <apu@escenic.com>
2020-02-14 18:38:10 +0600

    - Moved around the common dependencies

--- jproxy-client/src/main/java/experiment/loom/App.java
+++ jproxy-client/src/main/java/experiment/loom/App.java
@@ -2,6 +2,7 @@ package experiment.loom;
 
 public class App {
   public static void main(String[] args) {
-    Client.sendRequest();
+    System.out.println("Starting up client");
+    Client.sendRequest(args.length > 0 ? args[0] : null, args.length > 1 ? args[1] : null);
   }
 }
--- jproxy-client/src/main/java/experiment/loom/Client.java
+++ jproxy-client/src/main/java/experiment/loom/Client.java
@@ -1,7 +1,14 @@
 package experiment.loom;
 
+import com.google.common.base.Strings;
+
 public class Client {
-  public static void sendRequest() {
+  static final String HOST = "localhost";
+  static final int PORT = 5454;
+
+  public static void sendRequest(final String pHost, final String pPort) {
+    var host = Strings.isNullOrEmpty(pHost) ? HOST : pHost;
+    var port = Strings.isNullOrEmpty(pPort) ? PORT : Integer.parseInt(pPort);
     System.out.println("Send request");
   }
 }
